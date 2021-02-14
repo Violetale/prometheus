855742cb950f1d5667aea717fb4e32121699d51e
Svetlana Dmitrieva <53534598+Violetale@users.noreply.github.com>
2021-02-14 16:34:03 +0300

    Update ViewServiceProvider.php

--- src/Providers/ViewServiceProvider.php
+++ src/Providers/ViewServiceProvider.php
@@ -17,6 +17,22 @@ class ViewServiceProvider extends ServiceProvider
 
     public function boot()
     {
-
+        ViewFacade::composer("prometheus::admin.partials.sidebar",function (View $view){
+            $route =explode('.', Route::currentRouteName());
+            $target = $route[0] === 'admin' ? $route[1] : '';
+            $dashboards = "";
+            if($target=="dashboards"){
+                $dashboards = $route[2]==="main"?"main":"seo";
+            }
+            $view->with(compact('dashboards','target'));
+        });
+        ViewFacade::composer(["prometheus::admin.partials.sidebar","prometheus::admin.dashboards.main"],function (View $view) {
+            $newMainEvents=2;
+            $view->with(compact('newMainEvents'));
+        });
+        ViewFacade::composer(["prometheus::admin.partials.sidebar","prometheus::admin.dashboards.seo"],function (View $view) {
+            $newSeoEvents = 15;
+            $view->with(compact('newSeoEvents'));
+        });
     }
 }
