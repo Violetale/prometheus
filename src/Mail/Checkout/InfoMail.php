f48edad21b19356f4d052073ba6c9d0449d24da9
Svetlana Dmitrieva <53534598+Violetale@users.noreply.github.com>
2021-02-14 13:55:42 +0300

    Delete unused files.Update a few files.

--- src/Mail/Checkout/InfoMail.php
+++ src/Mail/Checkout/InfoMail.php
@@ -1,31 +1,13 @@
 <?php
 
-namespace Mediabroker\Core\Mail\Checkout;
+namespace Violetale\Prometheus\Mail\Checkout;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Mediabroker\Core\Models\Order\Order;
+use Violetale\Prometheus\Models\Order\Order;
 
 class InfoMail extends Mailable
 {
-    use Queueable, SerializesModels;
 
-    /** @var Order $order */
-    public $order;
-
-    /**
-     * @param Order $order
-     */
-    public function __construct(Order $order)
-    {
-        $this->order = $order;
-    }
-
-    public function build()
-    {
-        return $this->subject('Информация о заказе')
-            ->from(config('core-general.email_from'))
-            ->view('core::emails.checkout.info');
-    }
 }
--- src/Mail/Checkout/SuccessMail.php
+++ src/Mail/Checkout/SuccessMail.php
@@ -1,31 +1,12 @@
 <?php
 
-namespace Mediabroker\Core\Mail\Checkout;
+namespace Violetale\Prometheus\Mail\Checkout;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Mediabroker\Core\Models\Order\Order;
+use Violetale\Prometheus\Models\Order\Order;
 
 class SuccessMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
-    /** @var Order $order */
-    public $order;
-
-    /**
-     * @param Order $order
-     */
-    public function __construct(Order $order)
-    {
-        $this->order = $order;
-    }
-
-    public function build()
-    {
-        return $this->subject('Успешное оформление заказа')
-            ->from(config('core-general.email_from'))
-            ->view('core::emails.checkout.success');
-    }
 }
